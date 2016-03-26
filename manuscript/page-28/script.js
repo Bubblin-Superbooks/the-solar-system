13d9c70c64e1e2916c9ba451bb250eb12a335d41
Marvin Danig <marvin@marvindanig.com>
2016-03-26 13:09:46 -0400

    Responsifying sun shader

---
+++ manuscript/page-28/script.js
@@ -0,0 +1,12 @@
+window.onload = doALoadOfStuff;
+window.onresize = doALoadOfStuff;
+
+function doALoadOfStuff() {
+    var shader = document.getElementById('shadertoy');
+
+    var width = shader.clientWidth;
+
+    var height = width * 9 / 16;
+
+    shader.setAttribute("height", height + 'px');
+}
