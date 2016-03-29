99b79ed6738837514b23368a6322d1605710a216
Marvin Danig <marvin@marvindanig.com>
2016-03-28 23:10:13 -0400

    Sun is our star

--- manuscript/page-32/script.js
+++
@@ -1,9 +0,0 @@
-window.onload = doALoadOfStuff;
-window.onresize = doALoadOfStuff;
-
-function doALoadOfStuff() {
-    var shader = document.getElementById('shadertoy');
-    var width = shader.clientWidth;
-    var height = width * 9 / 16;
-    shader.setAttribute("height", height + 'px');
-}
