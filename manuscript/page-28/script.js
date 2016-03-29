a4e4a68fd2315e8cd3ec41c4fdb22c1eaaef2e21
Marvin Danig <marvin@marvindanig.com>
2016-03-29 18:36:47 -0400

    Eclipse

--- manuscript/page-28/script.js
+++ manuscript/page-28/script.js
@@ -3,8 +3,6 @@ window.onresize = doALoadOfStuff;
 
 function doALoadOfStuff() {
     var shader = document.getElementById('shadertoy');
-    // var width = shader.clientWidth;
-    // var height = width * 9 / 16;
     height = window.innerHeight;
     shader.setAttribute("height", height + 'px');
 }
