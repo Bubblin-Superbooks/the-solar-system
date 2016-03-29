9b13d1d8308e49dd7e8deabdbd3a7f94541b49e7
Marvin Danig <marvin@marvindanig.com>
2016-03-29 19:09:24 -0400

    window.height null fix for hidden iframes

--- manuscript/page-18/script.js
+++ manuscript/page-18/script.js
@@ -3,7 +3,8 @@ window.onresize = doALoadOfStuff;
 
 function doALoadOfStuff() {
     var shader = document.getElementById('shadertoy');
-    var width = shader.clientWidth;
+    var width = Math.max(shader.clientWidth || 300);
+
     var height = width * 7 / 16;
     shader.setAttribute("height", height + 'px');
 }
--- manuscript/page-28/script.js
+++ manuscript/page-28/script.js
@@ -3,6 +3,6 @@ window.onresize = doALoadOfStuff;
 
 function doALoadOfStuff() {
     var shader = document.getElementById('shadertoy');
-    height = window.innerHeight;
+    height = Math.max(document.documentElement.clientHeight, window.innerHeight || 300);
     shader.setAttribute("height", height + 'px');
 }
